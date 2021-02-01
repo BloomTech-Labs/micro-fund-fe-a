6bb68934c592b23405fb9531ec144cde93a2e90d
Daniel Hoesle <dhoesle21@gmail.com>
2021-02-01 13:46:35 -0700

    added styled components and basic styles

--- src/components/pages/Signup/RenderSignupPage.js
+++ src/components/pages/Signup/RenderSignupPage.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../../../images/microLogo.png';
+import styled from 'styled-components';
+
+const SignupStyle = styled.div`
+  h1 {
+    color: ${props => props.theme.primaryColor};
+  }
+  button {
+    background-color: ${props => props.theme.primaryColor};
+    color: white;
+  }
+`;
 
 function RenderSignupPage(props) {
   return (
     <div>
-      <h1>Sign Up for Microfund Here!</h1>
-      <form>
-        <h2>Register Here</h2>
-        <h4>Username:</h4>
-        <input name="username" type="text" />
-        <h4>Email:</h4>
-        <input name="email" type="email" />
-        <h4>Password:</h4>
-        <input name="password" type="password" />
+      <SignupStyle>
+        <h1>Sign Up for Microfund Here!</h1>
+        <form>
+          <h2>Register Here</h2>
+          <img src={logo} alt="MicroFund Logo" className="header-logo" />
+          <h4>Username:</h4>
+          <input name="username" type="text" />
+          <h4>Email:</h4>
+          <input name="email" type="email" />
+          <h4>Password:</h4>
+          <input name="password" type="password" />
 
-        <button>Join Microfund</button>
-      </form>
-      <p>already a member of Microfund?</p>
-      <h3>
-        {' '}
-        <Link to="/login"> Login Here</Link>{' '}
-      </h3>
+          <button>Join Microfund</button>
+        </form>
+        <p>already a member of Microfund?</p>
+        <h3>
+          {' '}
+          <Link to="/login"> Login Here</Link>{' '}
+        </h3>
+      </SignupStyle>
     </div>
   );
 }
